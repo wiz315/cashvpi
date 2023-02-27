4d1bf2d209bfb00a4c9666940f34bdc917d91664
wiz315 <jaradatm872@gmail.com>
2023-02-27 12:25:43 -0500

    fix2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
 
 if __name__ == "__main__": 
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8081, reload=True)
+    uvicorn.run("app.api:app", host="0.0.0.0", port=10000, reload=True)
     
